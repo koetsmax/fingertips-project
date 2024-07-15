042220fbfd466e5cbc1b4c74c00a863fcc05c465
Max Koets <koetsmax@gmail.com>
2024-07-15 12:42:41 +0200

    Formatting

--- fingertype.py
+++ fingertype.py
@@ -3,10 +3,10 @@ On July 1st the program called fingertips was shut down, which was a program tha
 This is a simple program that mimics the functionality of fingertips. But made in Python.
 """
 
-
 import keyboard
 import time
 from shortcuts import Shortcuts
+
 
 class Main:
     def __init__(self):
@@ -16,14 +16,27 @@ class Main:
     def on_key_press(self, event):
         if event.event_type == keyboard.KEY_UP:
             return
-        
+
         # Pressing these keys will clear the key list
         if event.name in ["enter", "tab"]:
             self.keys = []
             return
-        
-        modifier_keys = ["ctrl", "shift", "alt", "esc", "up", "down", "left", "right", "caps lock", "alt gr", "left windows", "delete"]
-        
+
+        modifier_keys = [
+            "ctrl",
+            "shift",
+            "alt",
+            "esc",
+            "up",
+            "down",
+            "left",
+            "right",
+            "caps lock",
+            "alt gr",
+            "left windows",
+            "delete",
+        ]
+
         # Dont log the key if it is a modifier key
         if event.name in modifier_keys:
             return
@@ -42,33 +55,32 @@ class Main:
                 # If the long text is a function, call it, so we can dynamically add a timestamp or similar
                 if callable(long_text):
                     long_text = long_text()
-                
+
                 # Clear the shortcut itself and the space
                 for _ in range(len(shortcut) + 1):  # +1 to remove the space as well
-                    keyboard.press_and_release('backspace')
-                
+                    keyboard.press_and_release("backspace")
+
                 # Short delay to ensure backspaces are processed before typing the long text
                 time.sleep(0.1)
-                
+
                 # Type the long text
                 keyboard.write(long_text + " ")
                 print(f"Shortcut detected: {shortcut} -> {long_text}")
-            
+
             # Reset keys buffer after processing space
             self.keys = []
-        
+
         # If the key is backspace, remove the last key from the list
         elif event.name == "backspace":
             try:
                 self.keys.pop()
-            except IndexError: # If the list is empty
+            except IndexError:  # If the list is empty
                 pass
         # Add the key to the list
         else:
             self.keys.append(event.name)
 
         print(self.keys)
-    
 
     def run(self):
         # Start the keyboard listener
